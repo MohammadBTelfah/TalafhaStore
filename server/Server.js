1d06969ae4130f989d2794b1e33732296ca2cf22
MohammadBTelfah <telfahmohammed51@gmail.com>
2025-08-24 21:38:18 +0300

    Update Server.js

--- server/Server.js
+++ server/Server.js
@@ -65,6 +65,7 @@ mongoose.connect(process.env.MONGO_URI, {
 });
 
 // ✅ Start Server
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
